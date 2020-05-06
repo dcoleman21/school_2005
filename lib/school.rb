90c1ab2f4bee544a16588e63e8e4a40cd56ce299
Dani Coleman <danicoleman00@gmail.com>
2020-05-06 14:12:07 -0600

    Add student name test

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,27 @@
 class School
-  attr_reader :start_time, :hours, :student_names
+  attr_reader :start_time,
+              :hours,
+              :student_names,
+              :name
 
   def initialize(start_time, hours)
-    @start_time = start_time
-    @hours = hours
+    @start_time = '9:00'
+    @hours = 7
     @student_names = []
+    @name = name
   end
 
   def hours_in_school_day
     @hours
   end
 
-  def  
+  def add_student_name(name)
+    @student_names.push(name)
+  end
 
+  def end_time
+    @hours + @start_time
+  end
 
 
 
