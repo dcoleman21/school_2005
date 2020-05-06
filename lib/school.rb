7e0242e936d020004732e0a387832f1154a7358f
Dani Coleman <danicoleman00@gmail.com>
2020-05-06 13:26:30 -0600

    Add iteration 1 test

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
+class School
+  attr_reader :start_time, :hours, :student_names
 
+  def initialize(start_time, hours)
+    @start_time = start_time
+    @hours = hours
+    @student_names = []
+  end
+
+  def hours_in_school_day
+    @hours
+  end
+
+  def  
+
+
+
+
+end
